9facd981d06c6742a7e70771b1d742d265c0a380
Vijayasekhar Deepak <Vijayasekhar.Deepak@walmart.com>
2021-07-05 21:02:13 +0530

    done

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:chat_app/widgets/auth/auth_form.dart';
+import 'package:flutter/services.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
+  void _submitAuthForm(String email, String password, String username,
+      bool isLogin, File image, BuildContext ctx) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      UserCredential userCredential;
+      if (isLogin) {
+        userCredential = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        userCredential = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        final ref = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child(userCredential.user!.uid + '.jpg');
+        await ref.putFile(image).then((_) {});
+        final url = await ref.getDownloadURL();
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+          'username': username,
+          'email': email,
+          'image_url': url,
+        });
+      }
+      print(userCredential.credential);
+    } on PlatformException catch (err) {
+      var message = 'An error occured, please check your credentials.';
+      if (err.message != null) {
+        message = err.message as String;
+      }
+
+      ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+      setState(() {
+        _isLoading = false;
+      });
+    } on Exception catch (err) {
+      ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+        content: Text(err.toString()),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(_submitAuthForm, _isLoading),
+    );
+  }
+}
---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/pickers/user_image_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AuthForm extends StatefulWidget {
+  final void Function(String email, String password, String userName,
+      bool isLogin, File image, BuildContext ctx) submitFn;
+  final bool isLoading;
+  AuthForm(
+    this.submitFn,
+    this.isLoading,
+  );
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+  File _userImageFile = File('');
+
+  void _pickedImage(File image) {
+    _userImageFile = image;
+  }
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      widget.submitFn(_userEmail, _userPassword, _userName, _isLogin,
+          _userImageFile, context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (!_isLogin) UserImagePicker(_pickedImage),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email address'),
+                    key: ValueKey('email'),
+                    autocorrect: false,
+                    textCapitalization: TextCapitalization.none,
+                    enableSuggestions: false,
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userEmail = value!;
+                    },
+                  ),
+                  if (!_isLogin)
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Username'),
+                      key: ValueKey('username'),
+                      autocorrect: true,
+                      textCapitalization: TextCapitalization.words,
+                      enableSuggestions: false,
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _userName = value!;
+                      },
+                    ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userPassword = value!;
+                    },
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  if (widget.isLoading) CircularProgressIndicator(),
+                  if (!widget.isLoading)
+                    ElevatedButton(
+                      onPressed: _trySubmit,
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                    ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(_isLogin
+                        ? 'Create new account'
+                        : 'I already have an account'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
